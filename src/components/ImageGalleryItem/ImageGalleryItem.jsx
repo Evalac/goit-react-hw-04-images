b401dfaf2728509b374702479aeccc86a6c3d3c4
Evalac <maslenokprograming.m@gmail.com>
2025-02-04 19:02:20 +0200

    ()

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,15 +3,17 @@ import css from './ImageGalleryItem.module.css';
 function ImageGalleryItem({ data }) {
   return (
     <>
-      {data.map((img, index) => (
-        <li key={index} className={css.ImageGalleryItem}>
-          <img
-            className={css.ImageGalleryItem_image}
-            src={img.webformatURL}
-            alt="pic"
-          />
-        </li>
-      ))}
+      {data.map((img, index) => {
+        return (
+          <li key={index} className={css.ImageGalleryItem}>
+            <img
+              className={css.ImageGalleryItem_image}
+              src={img.webformatURL}
+              alt="pic"
+            />
+          </li>
+        );
+      })}
     </>
   );
 }
